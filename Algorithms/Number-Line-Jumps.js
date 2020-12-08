645f134cb3a149dbec514708a1af80cacab89ecd
amirmohammad <mamad198585@gmail.com>
2020-12-08 11:50:40 +0330

    new qustion added

---
+++ Algorithms/Number-Line-Jumps.js
@@ -0,0 +1,16 @@
+function kangaroo(x1, v1, x2, v2) {
+    if(x1 > x2){
+        if(v2 > v1){
+            if((x1-x2) % (v2-v1) === 0){return "YES"}
+            return "NO"
+        }
+    }
+    else if(v1 > v2){
+        if((x2-x1) % (v1-v2) === 0){return "YES"}
+        return "NO"
+    }
+    return "NO"
+
+
+
+}
